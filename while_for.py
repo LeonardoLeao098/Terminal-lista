06fd68d3efe4ed9bd3bc641ac55b73f7b23e65a1
Leonardo Barboza Leão <leo.barboza7@gmail.com>
2026-02-03 19:28:18 -0300

    nova função: remover números da lista

--- while_for.py
+++ while_for.py
@@ -28,12 +28,33 @@ def listar():
             print(f'{contador}) {numero}')
             contador += 1
 
+def remover():
+    if not numeros:
+        print('A lista está vazia. Nada para rmemover')
+        return
+
+    listar()
+
+    while True:
+        try:
+            posicao = int(input('Digite a posição do número a ser removido: '))
+            if 1 <= posicao <= len(numeros):
+                removido = numeros.pop(posicao - 1)
+                print(f'Número {removido} removido com sucesso!')
+                break
+            else:
+                print('Posição inválida!')
+        except ValueError:
+            print('Digite apenas números!')
+                        
+
 def encerrar():
     print('Encerrando...')
      
 while True:
     print('\n1 - Adicionar número')
     print('2 - Listar números')
+    print('3 - Remover número')
     print('0 - Sair\n')
 
     escolha = ler_opcao()
@@ -42,6 +63,8 @@ while True:
         adicionar()
     elif escolha == 2:
         listar()
+    elif escolha == 3:
+        remover()
     elif escolha == 0:
         encerrar()
         break
